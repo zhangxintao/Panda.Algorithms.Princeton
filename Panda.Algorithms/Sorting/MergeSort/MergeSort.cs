291b9daeef145dcb2986483c9131d32903011350
xintao <zhangxintao621@outlook.com>
2016-04-09 22:30:59 +0800

    MergeSort Implementation

--- Panda.Algorithms/Sorting/MergeSort/MergeSort.cs
+++ Panda.Algorithms/Sorting/MergeSort/MergeSort.cs
@@ -31,14 +31,6 @@ namespace Sorting.MergeSort
 
         private void Merge(T[] a, T[] aux, int low, int mid, int high)
         {
-            Console.WriteLine("Before merge");
-            Console.WriteLine("low:{0}, mid:{1}, high:{2}", low, mid, high);
-            foreach(var item in a)
-            {
-                Console.Write(item.ToString() + " ");
-            }
-            Console.WriteLine();
-
             for (int i = low; i <= high; i++)
             {
                 aux[i] = a[i];
@@ -50,33 +42,21 @@ namespace Sorting.MergeSort
             {
                 if (j > mid)
                 {
-                    Console.WriteLine("in j > mid, j={0},mid={1},k={2}, index={3}", j, mid,k, index);
                     a[index] = aux[k++];
                 }
                 else if (k > high)
                 {
-                    Console.WriteLine("k > high, j={0},mid={1},k={2}, index={3}", j, mid, k, index);
                     a[index] = aux[j++];
                 }
-                else if (LessThan(a[j], a[k]))
+                else if (LessThan(aux[j], aux[k]))
                 {
-                    Console.WriteLine("LessThan(a[j], a[k]), j={0},mid={1},k={2}, index={3}", j, mid, k, index);
                     a[index] = aux[j++];
                 }
                 else
                 {
-                    Console.WriteLine("else, j={0},mid={1},k={2}, index={3}", j, mid, k, index);
-
                     a[index] = aux[k++];
                 }
             }
-
-            Console.WriteLine("After Merge");
-            foreach (var item in a)
-            {
-                Console.Write(item.ToString() + " ");
-            }
-            Console.WriteLine();
         }
 
         private bool LessThan(T item1, T item2)
